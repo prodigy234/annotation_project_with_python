346a0b2198f9bb81ec9e8f0a0ce4118f1573ec38
prodigy234 <k.gbenga234@gmail.com>
2025-01-03 05:22:21 +0100

    my new commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 # Database configuration
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:kajolagbenga@localhost/annotation_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@localhost/annotation_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
